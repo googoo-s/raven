46022a6735de16ab60ac42e6df94c28ddfc90179
sunyaoyao <sunyaoyao@kezaihui.com>
2023-06-15 17:08:13 +0800

    global using

---
+++ src/Raven.HttpApi/Common/BaseController.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Volo.Abp.AspNetCore.Mvc;
+
+namespace Raven.HttpApi.Common;
+
+public class BaseController : AbpController
+{
+
+}
