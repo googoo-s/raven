a1d677cb4d6067e641a428aaf7f0df526b6c9ab6
sunyaoyao <sunyaoyao@kezaihui.com>
2023-06-15 17:18:40 +0800

    add autoMapper

--- src/Raven.Application/RavenApplicationModule.cs
+++ src/Raven.Application/RavenApplicationModule.cs
@@ -1,13 +1,20 @@
 ﻿using Raven.Application.Contracts;
 using Raven.Domain;
+using Volo.Abp.AutoMapper;
 using Volo.Abp.Modularity;
 
 namespace Raven.Application;
 
 [DependsOn(
     typeof(RavenDomainModule),
-    typeof(RavenApplicationContractsModule)
+    typeof(RavenApplicationContractsModule),
+        typeof(AbpAutoMapperModule)
 )]
 public class RavenApplicationModule : AbpModule
 {
+    public override void ConfigureServices(ServiceConfigurationContext context)
+    {
+        base.ConfigureServices(context);
+        Configure<AbpAutoMapperOptions>(options => options.AddMaps<RavenApplicationModule>());
+    }
 }
--- src/Raven.HttpApi/RavenHttpApiModule.cs
+++ src/Raven.HttpApi/RavenHttpApiModule.cs
@@ -1,9 +1,16 @@
 using Raven.Application.Contracts;
 using Volo.Abp.Modularity;
+using Volo.Abp.AutoMapper;
 namespace Raven.HttpApi;
 [DependsOn(
-    typeof(RavenApplicationContractsModule)
+    typeof(RavenApplicationContractsModule),
+    typeof(AbpAutoMapperModule)
 )]
 public class RavenHttpApiModule : AbpModule
 {
+    public override void ConfigureServices(ServiceConfigurationContext context)
+    {
+        base.ConfigureServices(context);
+        Configure<AbpAutoMapperOptions>(options => options.AddMaps<RavenHttpApiModule>());
+    }
 }
